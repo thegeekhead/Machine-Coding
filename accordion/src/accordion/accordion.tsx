5e90bf0583e96c17ed5d512d06f94a6534d7fcc6
Anshansh Suman <anshansh.suman@zinnia.com>
2025-12-02 16:18:06 +0530

    accordion done

--- accordion/src/accordion/accordion.tsx
+++ accordion/src/accordion/accordion.tsx
@@ -1,6 +1,59 @@
+import { useState } from "react";
+import "./accordion.css";
+
 function Accordion() {
+
+    const accordionData = [
+        {
+            title: "Accordion Button 1",
+            content: "Accordion Content 1"
+        },
+        {
+            title: "Accordion Button 2",
+            content: "Accordion Content 2"
+        },
+        {
+            title: "Accordion Button 3",
+            content: "Accordion Content 3"
+        },
+        {
+            title: "Accordion Button 4",
+            content: "Accordion Content 4"
+        },
+        {
+            title: "Accordion Button 5",
+            content: "Accordion Content 5"
+        }
+    ];
+
+    const [accordionState, setAccordionState] = useState(null);
+
+    const accordionHandler = (index: any) => {
+        console.log(index);
+
+        setAccordionState(prevIndex => (prevIndex === index ? null : index));
+
+        // setAccordionState(accordionState === index ? null : index);
+
+        // if(accordionState === index) {
+        //     return setAccordionState(null);
+        // } else {
+        //     setAccordionState(index);
+        //     return;
+        // }
+    }
+
     return (
-        <>Hello</>
+        <>
+        {accordionData.map((item, index) => (
+            <div key={index}>
+            <button className="accordion-button" onClick={() => accordionHandler(index)}>{item.title}</button>
+            {accordionState === index && (
+                <div className="accordion-content">{item.content}</div>
+            )}
+            </div>
+        ))}
+        </>
     )
 }
 
